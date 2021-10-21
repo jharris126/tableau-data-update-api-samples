d573cd43b13ddafa27dabe80c98c22bb727a38a8
Jeremy Harris <jharris@coh.org>
2021-10-20 17:23:22 -0700

    fix auth variable change mismatch

--- basic/tableau_server_helper.py
+++ basic/tableau_server_helper.py
@@ -1,7 +1,7 @@
 from tableauserverclient import TableauAuth, Server
 
 
-auth = TableauAuth('my_tableau_login', 'my_tableau_password', site_id='my_tableau_site')
+tab_auth = TableauAuth('my_tableau_login', 'my_tableau_password', site_id='my_tableau_site')
 server = Server('https://10ax.online.tableau.com', use_server_version=True)
 new_ds_project = 'default'
 new_ds_name = 'Data Update API Test'
