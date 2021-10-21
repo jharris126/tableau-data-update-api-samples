96efd3f055484dfa6031312f4711f4e124f82a50
Jeremy Harris <jharris@coh.org>
2021-10-20 21:15:32 -0700

    refactor to use new TSC update_hyper_data

--- basic/basic_incremental_load.py
+++ basic/basic_incremental_load.py
@@ -1,45 +1,7 @@
 import tableauserverclient as TSC
 from tableauhyperapi import HyperProcess, Connection, Telemetry, CreateMode, SchemaName, TableName, escape_name
-import requests
-import json
 from tableau_server_helper import tab_auth, server, new_ds_project, new_ds_name
 import uuid
-
-
-# does magic around chunk for sending hyper data to Tableau Server
-def make_multipart(parts):
-    mime_multipart_parts = []
-    for name, (filename, blob, content_type) in parts.items():
-        multipart_part = requests.packages.urllib3.fields.RequestField(name=name, data=blob, filename=filename)
-        multipart_part.make_multipart(content_type=content_type)
-        mime_multipart_parts.append(multipart_part)
-
-    post_body, content_type = requests.packages.urllib3.filepost.encode_multipart_formdata(mime_multipart_parts)
-    content_type = "".join(("multipart/mixed",) + content_type.partition(";")[1:])
-    return post_body, content_type
-
-
-# opens the hyper file and PUTs it to Tableau Server
-def upload_hyper_data(upload_id, incr_file):
-    chunk_size = 1024 * 1024 * 5  # 5MB chunks
-    put_url = f'{server.baseurl}/sites/{server.site_id}/fileUploads/{upload_id}'
-
-    with open(incr_file, 'rb') as f:
-        while True:
-            data = f.read(chunk_size)
-            if not data:
-                break
-            payload, content_type = make_multipart(
-                {
-                    "request_payload": ('', '', 'text/xml'),
-                    "tableau_file": ('file', data, 'application/octet-stream'),
-                }
-            )
-            requests.put(
-                put_url,
-                data=payload,
-                headers={'X-Tableau-Auth': server.auth_token, 'content-type': content_type},
-            )
 
 
 # Create a New Hyper File with the the same schema as the previous but add 1 row to be updated and 1 to be inserted
@@ -48,10 +10,12 @@ def create_incr_hyper(incr_file):
         with Connection(endpoint=hyper.endpoint,
                         database=incr_file,
                         create_mode=CreateMode.CREATE_AND_REPLACE) as connection:
-            sql = f"CREATE TABLE {SchemaName('public')}.{TableName('test')} AS "
-            sql += f"SELECT 'foo' AS {escape_name('colA')}, 3 AS {escape_name('colB')} "
-            sql += "UNION ALL "
-            sql += f"SELECT 'bar' AS {escape_name('colA')}, 2 AS {escape_name('colB')}"
+            sql = (
+                f"CREATE TABLE {SchemaName('public')}.{TableName('test')} AS "
+                f"SELECT 'foo' AS {escape_name('colA')}, 3 AS {escape_name('colB')} "
+                "UNION ALL "
+                f"SELECT 'bar' AS {escape_name('colA')}, 2 AS {escape_name('colB')}"
+            )
 
             connection.execute_command(sql)
 
@@ -59,19 +23,17 @@ def create_incr_hyper(incr_file):
 # use data update api to upsert the new records into the published hyper file on Tableau Server
 def publish_incr_hyper(incr_file):
 
-    # action batch descriptors translate to Hyper SQL DDL and tell Tableau Server how to upsert the new data
-    actions = {
-        'actions': [
-            {
-                'action': 'upsert',
-                'target-table': 'test',
-                'target-schema': 'public',
-                'source-table': 'test',
-                'source-schema': 'public',
-                'condition': {'op': 'eq', 'target-col': 'colA', 'source-col': 'colA'}
-            }
-        ]
-    }
+    # action batch descriptors translate to Hyper SQL DDL and tells Tableau Server how to upsert the new data
+    actions = [
+        {
+            'action': 'upsert',
+            'target-table': 'test',
+            'target-schema': 'public',
+            'source-table': 'test',
+            'source-schema': 'public',
+            'condition': {'op': 'eq', 'target-col': 'colA', 'source-col': 'colA'}
+        }
+    ]
 
     with server.auth.sign_in(tab_auth):
         req_option = TSC.RequestOptions()
@@ -82,26 +44,14 @@ def publish_incr_hyper(incr_file):
                                          TSC.RequestOptions.Operator.Equals,
                                          new_ds_project))
         matching_datasources, pagination_item = server.datasources.get(req_option)
-        ds_id = matching_datasources[0].id
+        ds = matching_datasources[0]
 
-        # get tableau auth token, must use JSON content-type, add a unique request ID
-        header = {
-            'X-Tableau-Auth': server.auth_token,
-            'content-type': 'application/json',
-            'accept': 'application/json',
-            'RequestID': str(uuid.uuid4())
-        }
+        # create a unique upload ID for the request
+        req_id = str(uuid.uuid4())
 
-        # create an upload session ID for large file uploads
-        up_endpt = f'{server.baseurl}/sites/{server.site_id}/fileUploads/'
-        upload_id = requests.post(up_endpt, headers=header).json()['fileUpload']['uploadSessionId']
-
-        upload_hyper_data(upload_id, incr_file)
-
-        # put together a patch request that passes the header, actions, hyper file, and upload ID to Tableau Server
-        endpt = f'{server.baseurl}/sites/{server.site_id}/datasources/{ds_id}/data?uploadSessionId={upload_id}'
-        response = requests.patch(endpt, json.dumps(actions), headers=header).json()
-        print(response)
+        # TSC update_hyper_data takes in the datasource item, upload ID, actions, and hyper file and pushes to server
+        response = server.datasources.update_hyper_data(ds, request_id=req_id, actions=actions, payload=incr_file)
+        print('Update Data Job luid: ' + str(response.id))
 
 
 def main():
